0ea6d214934cb6222c0ab9b726ab27b22bb3376a
noway <dragonfury1928@gmail.com>
2024-02-29 18:05:08 -0500

    Add: Basic Material UI usage

--- packages/web-app/src/App.js
+++ packages/web-app/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	      <Button variant="outlined">Hello world</Button>
       </header>
     </div>
   );
