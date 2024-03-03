8da14c629e9d5450b7c0378186296c02f929918f
ryanglatour <ryanglatour@gmail.com>
2024-03-03 00:20:09 -0500

    Moved upload route to routes.js

--- packages/shared/api/routes/routes.js
+++ packages/shared/api/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const UserController = require('../controllers/userController');
 const PostController = require('../controllers/postController');
@@ -29,5 +30,27 @@ router.get('/getposts', PostController.getPosts)
 
 // delete post route
 router.delete('/deletepost', PostController.deletePost)
+
+// multer stuffs
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './uploads'); // Specify the directory where you want to store the uploaded files
+		},
+		filename: function (req, file, cb) {
+			cb(null, Date.now() + '-' + file.originalname); // Use a unique filename to avoid overwriting
+		},
+	});
+	  
+	const upload = multer({ storage: storage });
+	
+// Express route for handling file uploads
+router.post('/upload', upload.single('image'), (req, res) => {
+	// Access uploaded file information using req.file
+	if (!req.file) {
+	return res.status(400).send('No file uploaded.');
+	}
+
+	res.send('File uploaded!');
+});
 
 module.exports = router;
--- packages/shared/server.js
+++ packages/shared/server.js
@@ -3,10 +3,9 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const multer = require('multer');
 const cors = require('cors');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 mongoose.connect(process.env.MONGO_URL)
 
 
@@ -29,29 +28,6 @@ app.use(express.json());
 
 // Routes -> defined in shared
 app.use('/', require('./api/routes/routes.js'));
-
-// multer stuffs
-const storage = multer.diskStorage({
-destination: function (req, file, cb) {
-	cb(null, 'uploads/'); // Specify the directory where you want to store the uploaded files
-	},
-	filename: function (req, file, cb) {
-		cb(null, Date.now() + '-' + file.originalname); // Use a unique filename to avoid overwriting
-	},
-});
-  
-const upload = multer({ storage: storage });
-
-// Express route for handling file uploads
-app.post('/upload', upload.single('image'), (req, res) => {
-	// Access uploaded file information using req.file
-	if (!req.file) {
-	return res.status(400).send('No file uploaded.');
-	}
-
-	res.send('File uploaded!');
-});
-
 
 // Start the server
 app.listen(PORT, () => {
