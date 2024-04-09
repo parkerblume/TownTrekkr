c399fea10f08b4420fc78cae8ff0f483dd4a503d
gscarboro <scarborogarrison@gmail..com>
2024-04-09 13:47:27 -0400

    available towns list

--- packages/web-app/src/components/TownsList.js
+++ packages/web-app/src/components/TownsList.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { colors } from '../styles/commonStyles';
 
 const example = [
-  {
-    _id: "1",
-    name: "Example-ville",
-    description: "A fake town with fake people and a fake name.",
-    creatingUsername: "Example_User_10",
-    topLeftLat: 0.0,
-    topLeftLong: 1.0,
-    botRightLat: -1.0,
-    botRightLong: 2.0,
-    scoreMod: 1,
-    townMembers: [{ userId: "user1", _id: "member1" }],
-  },
-  {
-    _id: "2",
-    name: "Fake Town",
-    description: "We're way faker than that other one!",
-    creatingUsername: "Example_User_11",
-    topLeftLat: 0.1,
-    topLeftLong: 1.1,
-    botRightLat: -1.1,
-    botRightLong: 2.1,
-    scoreMod: 1,
-    townMembers: [{ userId: "user1", _id: "member1" }],
-  }
+  // {
+  //   _id: "1",
+  //   name: "Example-ville",
+  //   description: "A fake town with fake people and a fake name.",
+  //   creatingUsername: "Example_User_10",
+  //   topLeftLat: 0.0,
+  //   topLeftLong: 1.0,
+  //   botRightLat: -1.0,
+  //   botRightLong: 2.0,
+  //   scoreMod: 1,
+  //   townMembers: [{ userId: "user1", _id: "member1" }],
+  // },
+  // {
+  //   _id: "2",
+  //   name: "Fake Town",
+  //   description: "We're way faker than that other one!",
+  //   creatingUsername: "Example_User_11",
+  //   topLeftLat: 0.1,
+  //   topLeftLong: 1.1,
+  //   botRightLat: -1.1,
+  //   botRightLong: 2.1,
+  //   scoreMod: 1,
+  //   townMembers: [{ userId: "user1", _id: "member1" }],
+  // }
 ];
 
 const TownsList = () => {
@@ -108,9 +109,10 @@ const TownsList = () => {
   }, [fetchTowns]);
 
   return (
-    <div style={{ background: '#ABC4AB', padding: '20px', height: '60vh', width: '35vw', overflowY: 'auto', scrollbarWidth: 'thin', scrollbarColor: '#A39171 #ABC4AB' }}>
+    <div style={{ background: '#ABC4AB', padding: '20px', height: '60vh', width: '30vw', overflowY: 'auto', scrollbarWidth: 'thin', scrollbarColor: '#A39171 #ABC4AB' }}>
+      <h2 style={{ fontSize: '36px', color: colors.buttonPrimary, textAlign: 'center' }}>My Towns</h2>
       <button onClick={fetchTowns} disabled={isLoading} style={{ padding: '10px 20px', fontSize: '18px', backgroundColor: '#DCC9B6', margin: '10px', borderRadius: '5px' }}>
-        {isLoading ? 'Loading...' : 'Refresh Towns'}
+        {isLoading ? 'Loading...' : 'Refresh'}
       </button>
       {towns.map(town => (
         <div key={town._id} style={{ marginBottom: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#A39171', borderRadius: '10px', padding: '15px', minWidth: '500px' }}>
